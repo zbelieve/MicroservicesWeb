a6708d6aeb5c168c3e4098d6954970d5d2038630
zbelieve <921365915@qq.com>
2020-04-28 19:50:24 +0800

    commit4-28

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
@@ -3,11 +3,14 @@ package com.xuecheng.manage_cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+
+@EnableDiscoveryClient//一个EurekaClient从EurekaServer发现服务
 //这个注解会扫描这个启动类所在的包以及其子包
 @SpringBootApplication
 @EntityScan("com.xuecheng.framework.domain.cms")//扫描实体类
